95d5596518c6f455eb550482752499df8d4ccdc7
Nilayan <nilayanbose14@gmail.com>
2021-05-13 19:59:19 +0530

    update

--- NeighborlyAPI/getPost/__init__.py
+++ NeighborlyAPI/getPost/__init__.py
@@ -10,7 +10,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://testdb-cosmos:K3fAnG4QP6hOkxpu8dczkwyLwEY2pZjsbKwpxVAm8FqEqIb9I6SL1JpjtbJ9qTNCXjRbAO9FA2bFAAPLTAl5CA==@testdb-cosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@testdb-cosmos@"  # TODO: Update with appropriate MongoDB connection information
+            url = "#################################################################################################"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['db']
             collection = database['posts']
